b693d1c75304031017a622903125ea622421f22f
Ike Holy Destiny <ikedestiny18@gmail.com>
2023-03-17 15:41:06 +0300

    used templates to make two not yet connected web pages; need to work on my html

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,11 +4,12 @@ from listings.models import band , band_title
 
 def Hello(request):
     bands = band.objects.all()
-    titles = band_title.objects.all()
+    
     return render(request,'listings/hello.html',{'bands':bands})
 
 def about(request):
     return HttpResponse('<h1>About-us</h1>  <p>We Love Merch!!</p>')
 
 def goods(request):
-    return HttpResponse("<p>That’s a great question.  Django takes care of the rest.</p>")
+    titles = band_title.objects.all()
+    return render(request,'listings/goods.html',{'titles':titles})
