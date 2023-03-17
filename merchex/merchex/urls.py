0dd07b93438819c84a4b308b81a0dd26196a2967
Ike Holy Destiny <ikedestiny18@gmail.com>
2023-03-17 15:22:01 +0300

    migratons are really nice

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Hello/', views.Hello),
+    path('about-us/', views.about),
+    path('goods/', views.goods)
 ]
